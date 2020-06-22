c3d98ad6e5e0e8ad219e181beef07fe6bae792a5
Peterturn <55192090+Peterturn@users.noreply.github.com>
2020-06-22 14:57:46 +0200

    adding info, dynamic content in the pugs
    added dynmic content in the pugs as well as trouble shooting some bad file paths. added generic info to the about section.

--- 6-static-node-and-express-site-v1/app.js
+++ 6-static-node-and-express-site-v1/app.js
@@ -4,11 +4,39 @@ const { projects } = require('./data.json');
 app.set('view engine', 'pug');
 app.use( '/static', express.static('public') );
 
+/* GET home page. */
 app.get('/', function(req, res, next) {
     res.render( 'index', { projects } );
 });
 
+/* GET about page. */
 app.get('/about', function(req, res, next) {
     res.render( 'about', { projects } );
 });
+
+/* GET recipe page. */
+app.get('/projects/:id', function(req, res, next) {
+    const projectId = req.params.id;
+    const project = projects.find( ({ id }) => id === +projectId );
+    
+    if (project) {
+      res.render('project', { project });
+    } else {
+      res.sendStatus(404);
+    }
+  });
+
+  // catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    res.status(err.status || 500);
+    res.render('error');
+  });
 app.listen(3000);
