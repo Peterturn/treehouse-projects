8b5d2911ec4a25d94cedf44edb834799d3dc7f8e
Peterturn <55192090+Peterturn@users.noreply.github.com>
2020-06-22 13:47:40 +0200

    added routes and basic info
    Routes for pages added. Required json. added profile info to pug templates. created route for about button.

--- 6-static-node-and-express-site-v1/app.js
+++ 6-static-node-and-express-site-v1/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
-
+const { projects } = require('./data.json');
 app.set('view engine', 'pug');
 app.use( '/static', express.static('public') );
 
+app.get('/', function(req, res, next) {
+    res.render( 'index', { projects } );
+});
+
+app.get('/about', function(req, res, next) {
+    res.render( 'about', { projects } );
+});
 app.listen(3000);
