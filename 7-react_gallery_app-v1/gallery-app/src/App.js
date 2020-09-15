bfdd21b80fa93a8239d059bd671a456ebd4c8503
Peterturn <55192090+Peterturn@users.noreply.github.com>
2020-09-15 17:22:09 +0200

    Update App.js

--- 7-react_gallery_app-v1/gallery-app/src/App.js
+++ 7-react_gallery_app-v1/gallery-app/src/App.js
@@ -3,28 +3,33 @@ import './App.css';
 import SearchForm from './SearchForm';
 import Nav from './Nav';
 import Gallery from './Gallery';
-//import { apiKey } from './Key';
+import axios from 'axios';
+import { apiKey } from './Key';
 
  class App extends Component {
-  
-  constructor() {
-    super();
+
+  constructor(props) {
+    super(props);
     this.state = {
       photos: [],
       loading: true
     };
   } 
 
-    getSearchData = () => {
-      fetch(`http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC`)
-        .then(response => response.json())
-        .then(responseData => {
-          this.setState({ photos: responseData.data });
-        })
-        .catch(error => {
-          console.log('Error fetching and parsing data', error);
-        });
-    }
+  getSearchData(query = "cats") {
+    console.log('hi');
+   axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+   .then(response => {
+     console.log(response.data);
+     this.setState({
+       photos: response.data.photos.photo,
+       loading: true
+     });
+   })
+   .catch(error => {
+   console.log('Error fetching and parsing data', error);
+   });
+  }
   render (){
     console.log(this.state.photos);
     return (
