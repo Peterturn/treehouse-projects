15cda518e58c3ac2686075d166c03d39aca86cea
Peterturn <55192090+Peterturn@users.noreply.github.com>
2020-09-16 12:11:39 +0200

    get ingnore change

--- 7-react_gallery_app-v1/gallery-app/src/App.js
+++ 7-react_gallery_app-v1/gallery-app/src/App.js
@@ -1,35 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import SearchForm from './SearchForm';
 import Nav from './Nav';
 import Gallery from './Gallery';
-import axios from 'axios';
+
 import { apiKey } from './Key';
 
  class App extends Component {
-
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       photos: [],
       loading: true
     };
   } 
 
-  getSearchData(query = "cats") {
-    console.log('hi');
-   axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-   .then(response => {
-     console.log(response.data);
-     this.setState({
-       photos: response.data.photos.photo,
-       loading: true
-     });
-   })
-   .catch(error => {
-   console.log('Error fetching and parsing data', error);
-   });
-  }
+    getSearchData() {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cat&per_page=10&page=1&format=json&nojsoncallback=1`)
+    .then(response => {
+      this.setState({
+        photos: response.data.photos.photo,
+        loading: false,
+      });
+    })
+    //  .catch(error => {
+    //  console.log('Error fetching and parsing data', error);
+    //  });
+    }
   render (){
     console.log(this.state.photos);
     return (
--- 7-react_gallery_app-v1/gallery-app/src/Key.js
+++ 7-react_gallery_app-v1/gallery-app/src/Key.js
@@ -1,3 +1,3 @@
 
-export const apiKey = 'be062c97a8705f46019b77f666db9c5d'
+export const apiKey = 'eb03e3e18348e37bf2bc9e4079cb8e50'
 
