8e5e657e579f273cad4f06ab4f30d0d7d1a456a8
Peterturn <55192090+Peterturn@users.noreply.github.com>
2020-09-17 09:51:32 +0200

    working on gif population

--- 7-react_gallery_app-v1/gallery-app/src/App.js
+++ 7-react_gallery_app-v1/gallery-app/src/App.js
@@ -16,25 +16,27 @@ import { apiKey } from './Key';
     };
   } 
 
-  getSearchData() {
+  componentDidMount() {
   axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cat&per_page=10&page=1&format=json&nojsoncallback=1`)
   .then(response => {
+
     this.setState({
       photos: response.data.photos.photo,
       loading: false,
     });
   })
-    //  .catch(error => {
-    //  console.log('Error fetching and parsing data', error);
-    //  });
+     .catch(error => {
+     console.log('Error fetching and parsing data', error);
+     });
     }
+  
   render (){
     console.log(this.state.photos);
     return (
       <div className="container">
         <SearchForm />
         <Nav />
-        <Gallery data={this.state.photos}/>
+        <Gallery value={this.state.photos}/>
 
       </div>
     )
